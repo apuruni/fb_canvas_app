20129b9c803afa9c355635428d3f43c1803e587a
Apuruni <apuruni@gmail.com>
2015-10-07 17:44:18 +0800

    invite

--- game_requests.php
+++ game_requests.php
@@ -13,8 +13,25 @@
           console.log(response);
       });
     }
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1608578172737796',
+        xfbml      : true,
+        version    : 'v2.4'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
   </script>
+
   <h1>Game requests</h1>
-  <input type='button' onclick='invite();'>Invite Friends</input>
+  <input type='button' onclick='invite();' value='Invite Friends'/>
  </body>
 </html>
