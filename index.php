c8ea57f4da2fc88936f1098ab7285eb1877ce9d9
Apuruni <apuruni@gmail.com>
2015-10-07 16:36:24 +0800

    update

--- index.php
+++ index.php
@@ -1,97 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>AdminLTE | Dashboard</title>
-        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <!-- bootstrap 3.0.2 -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <!-- font Awesome -->
-        <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <!-- Ionicons -->
-        <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
-        <!-- Theme style -->
-        <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
-
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
-    </head>
-    <body class="skin-blue">
-        <!-- header logo: style can be found in header.less -->
-        <header class="header">
-            <a href="index.php" class="logo">
-                Test Canvas App
-            </a>
-        </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    Welcome!
-                    <small>main menu</small>
-                </h1>
-            </section>
-
-            <!-- Main content -->
-            <section class="content">
-              <div class="col-md-6">
-                  <!-- Primary tile -->
-                  <div class="box box-solid bg-light-blue">
-                      <div class="box-header">
-                          <h3 class="box-title">Primary Tile</h3>
-                      </div>
-                      <div class="box-body">
-                          <button class="btn btn-info btn-flat">Login</button>
-                      </div><!-- /.box-body -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-              <div class="col-md-6">
-                  <!-- Primary tile -->
-                  <div class="box box-solid bg-light-blue">
-                      <div class="box-header">
-                          <h3 class="box-title">Primary Tile</h3>
-                      </div>
-                      <div class="box-body">
-                          <button class="btn btn-info btn-flat">Login</button>
-                      </div><!-- /.box-body -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-              <div class="col-md-6">
-                  <!-- Primary tile -->
-                  <div class="box box-solid bg-light-blue">
-                      <div class="box-header">
-                          <h3 class="box-title">Primary Tile</h3>
-                      </div>
-                      <div class="box-body">
-                          <button class="btn btn-info btn-flat">Login</button>
-                      </div><!-- /.box-body -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-              <div class="col-md-6">
-                  <!-- Primary tile -->
-                  <div class="box box-solid bg-light-blue">
-                      <div class="box-header">
-                          <h3 class="box-title">Primary Tile</h3>
-                      </div>
-                      <div class="box-body">
-                          <button class="btn btn-info btn-flat">Login</button>
-                      </div><!-- /.box-body -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-            </section><!-- /.content -->
-        </div><!-- ./wrapper -->
-
-
-        <!-- jQuery 2.0.2 -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-        <!-- Bootstrap -->
-        <script src="../js/bootstrap.min.js" type="text/javascript"></script>
-        <!-- AdminLTE App -->
-        <script src="../js/AdminLTE/app.js" type="text/javascript"></script>
-
-    </body>
+<head>
+  <title>Test App</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+  <div>
+    <ui>
+      <li><a href="login_onload.php">Login onload</a></li>
+      <li><a href="login_button.php">Login - using Facebook login button</a></li>
+    </ui>
+  </div>
+</body>
 </html>
