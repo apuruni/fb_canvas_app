ed5350a8106f117c638777c24eb0687a01f7899c
Kun Wang <kun@fb.com>
2015-10-07 11:21:34 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Platform Test</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<h1>Kun's Canvas App</h1>
+
+</body>
+</html>
