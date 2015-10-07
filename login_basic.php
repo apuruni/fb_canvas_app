dd9078d4db6a19c03df830d80987faeff066a4ec
Apuruni <apuruni@gmail.com>
2015-10-07 15:53:52 +0800

    add bootstrap fw

---
+++ login_basic.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Platform Test</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+  <script>
+    function onLogin(response) {
+      if (response.status == 'connected') {
+        FB.api('/me?fields=first_name', function(data) {
+          var welcomeBlock = document.getElementById('fb-welcome');
+          welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+        });
+      }
+    }
+
+    function checkLoginState() {
+      FB.getLoginStatus(function(response) {
+        // Check login status on load, and if the user is
+        // already logged in, go directly to the welcome message.
+        if (response.status == 'connected') {
+          onLogin(response);
+        } else {
+          // Otherwise, show Login dialog first.
+          FB.login(function(response) {
+            onLogin(response);
+          }, {scope: 'user_friends, email'});
+        }
+      });
+    }
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1608578172737796',
+        xfbml      : true,
+        version    : 'v2.4'
+      });
+
+      checkLoginState();
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+   </script>
+
+   <h1>Kun's Canvas App</h1>
+   <h2 id="fb-welcome"></h2>
+   <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+   </fb:login-button>
+ </body>
+</html>
