fadcb636af1c2debeb9908281fa23e43f19a6edc
Apuruni <apuruni@gmail.com>
2017-01-12 12:23:54 +0900

    log

--- post_photo.php
+++ post_photo.php
@@ -9,7 +9,7 @@ session_start();
 $fb = new Facebook\Facebook($app_params);
 $photo_file_path = __DIR__ .'/img/pic_720p.png';
 
-echo 'Sharing photo path: '. $photo_file_path;
+echo 'Sharing photo path: '. $photo_file_path . "\n";
 
 
 $data = [
@@ -21,10 +21,10 @@ try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->post('/me/photos', $data, 'EAAZA2BpuLIk0BAMJt6Mn7OWdqlR9cZCzEJsFLH3ZAuO0z0uBvwUUUZB7F0FqT6PIZBhOafnruLihP9TMJWyGlFOwD1ZAaFnj4SU0qlw2JwMyryki1bTXwdtZBRpn7QB34ibYhyTgCZBIxZCyTrd3klESmxAZC77izcodfuzu659B0y791Lv7XOoqLVuPYWFK8mVFfQYY54l4eECwZDZD');
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  echo 'Graph returned an error: ' . $e->getMessage();
+  echo 'Graph returned an error: ' . $e->getMessage()  . "\n";
   exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  echo 'Facebook SDK returned an error: ' . $e->getMessage()  . "\n";
   exit;
 } catch (Exception $e) {
   echo 'error: ' . $e->getMessage();
@@ -33,6 +33,6 @@ try {
 
 $graphNode = $response->getGraphNode();
 
-echo 'Photo ID: ' . $graphNode['id'];
+echo 'Photo ID: ' . $graphNode['id']  . "\n";
 ?>
 
